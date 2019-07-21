9dc920c629c431b6b5a524a01ddb23775781f03e
Joshef Hossain Bappy <hossainjoshef@gmail.com>
2019-07-21 11:10:54 +0600

    21 july

--- sells/views.py
+++ sells/views.py
@@ -13,13 +13,22 @@ def admin_dashboard(request):
     return render(request,'sells/admin/index.html')
 
 def registration(request):
-    
+    if request.method=="POST":
+        name          = request.POST['name']
+        email         = request.POST['email']
+        mobile        = request.POST['mobile']
+        address       = request.POST['address']
+
+        chk_user = models.Registration.objects.filter(mobile = mobile)
+        if not chk_user:
+            models.Registration.objects.create(name = name, email = email, mobile = mobile, password = mobile, address = address)
+            messages.success(request,"Success!") 
+        else:
+            messages.warning(request,"Mobile number is already exits!")    
+        
     return render(request,'sells/admin/registration.html')
 
 def Login(request):
     
     return render(request,'sells/page-login.html')
-
-def Login1(request):
     
-    return render(request,'sells/page-login.html')
